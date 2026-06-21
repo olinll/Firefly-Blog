08a002474f07a6d8261e530c889f14022de0f115
顾拾柒 <olinl@foxmail.com>
2026-06-21 19:00:32 +0800

    fix(friendsConfig): 移除重复的友链条目

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -145,27 +145,6 @@ export const friendsConfig: FriendLink[] = [
 		tags: ["Blog"],
 		weight: 1,
 		enabled: true,
-		source: "qq:9892214",
-	},
-	{
-		title: "Hyde Blog",
-		imgurl:
-			"https://seasir.top/assets/avatar.avif",
-		desc: "人心中的成见是一座大山",
-		siteurl: "https://seasir.top/",
-		tags: ["Blog"],
-		weight: 1,
-		enabled: true,
-	},
-	{
-		title: "Hyde Blog",
-		imgurl:
-			"https://seasir.top/assets/avatar.avif",
-		desc: "人心中的成见是一座大山",
-		siteurl: "https://seasir.top/",
-		tags: ["Blog"],
-		weight: 1,
-		enabled: true,
 	},
 ];
 
